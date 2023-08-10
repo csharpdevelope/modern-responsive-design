b43f9b907d1064eb28beab067c73a79417ffe6e7
admin <drakhimov001@gmail.com>
2023-08-10 23:47:13 +0500

    second-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './app.css';
+import { Navbar } from './components';
+
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className=''>
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
